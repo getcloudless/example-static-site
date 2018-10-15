76488c9572488a872cb0cc7c465b5ff62d83e737
Shaun Verch <shaun@getcloudless.com>
2018-10-15 14:11:46 -0400

    Spin up Consul dependency for service
    This creates a Consul server and adds a dummy key as part of a test run,
    so that we can ensure the configuration on the web server that needs to
    talk to Consul works properly.

--- blueprint_fixture.py
+++ blueprint_fixture.py
@@ -1,13 +1,15 @@
 """
-Apache Test Fixture
-
-This fixture doesn't do any setup, but verifies that the created service is
-running default apache.
+Static Site Test Fixture
 """
+import os
+from retrying import retry
 import requests
 from cloudless.testutils.blueprint_tester import call_with_retries
 from cloudless.testutils.fixture import BlueprintTestInterface, SetupInfo
 from cloudless.types.networking import CidrBlock
+import consul
+
+SERVICE_BLUEPRINT = os.path.join(os.path.dirname(__file__), "example-consul/blueprint.yml")
 
 RETRY_DELAY = float(10.0)
 RETRY_COUNT = int(6)
@@ -20,16 +22,44 @@ class BlueprintTest(BlueprintTestInterface):
         """
         Create the dependent services needed to test this service.
         """
-        # Since this service has no dependencies, do nothing.
-        return SetupInfo({}, {})
+        # Create consul since our web server needs it to pull API keys.
+        service_name = "consul"
+        service = self.client.service.create(network, service_name, SERVICE_BLUEPRINT, count=1)
+
+        @retry(wait_fixed=5000, stop_max_attempt_number=24)
+        def add_dummy_api_keys(service):
+            public_ips = [i.public_ip for s in service.subnetworks for i in s.instances]
+            assert public_ips, "No services are running..."
+            for public_ip in public_ips:
+                consul_client = consul.Consul(public_ip)
+                if consul_client.kv.get('dummy_api_key'):
+                    continue
+                consul_client.kv.put('dummy_api_key', 'dummy_api_key_value')
+            return True
+
+        # Now let's add some dummy API keys to Consul.
+        my_ip = requests.get("http://ipinfo.io/ip")
+        test_machine = CidrBlock(my_ip.content.decode("utf-8").strip())
+        self.client.paths.add(test_machine, service, 8500)
+        add_dummy_api_keys(service)
+        self.client.paths.remove(test_machine, service, 8500)
+
+        return SetupInfo(
+            {"service_name": service_name},
+            {"consul_ips": [i.private_ip for s in service.subnetworks for i in s.instances]})
 
     def setup_after_tested_service(self, network, service, setup_info):
         """
         Do any setup that must happen after the service under test has been
         created.
         """
-        internet = CidrBlock("0.0.0.0/0")
-        self.client.paths.add(internet, service, 80)
+        consul_service_name = setup_info.deployment_info["service_name"]
+        consul_service = self.client.service.get(network, consul_service_name)
+        my_ip = requests.get("http://ipinfo.io/ip")
+        test_machine = CidrBlock(my_ip.content.decode("utf-8").strip())
+        self.client.paths.add(service, consul_service, 8500)
+        self.client.paths.add(test_machine, service, 80)
+        self.client.paths.add(test_machine, service, 443)
 
     def verify(self, network, service, setup_info):
         """
